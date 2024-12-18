8e56630c1e4c0d75c454bd2d8111d5bae273c170
timotewb <timote.wb@gmail.com>
2024-12-18 20:16:21 +1300

    udpate greeting message with bold for dev env

--- terminal/app/greeting.go
+++ terminal/app/greeting.go
@@ -19,7 +19,7 @@ To get started, try entering the command <span class='hst-command'>help</span> a
 
 Session start time: %v
 
--- Development Environment --`
+<b>-- Development Environment --</b>`
 
 		resp_string  = fmt.Sprintf(resp_string, formattedTime)
 	} else {
